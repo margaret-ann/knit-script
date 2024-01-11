0947cbce0e2c534f659e991981cef97700164ecb
Meg Hardin <mhardin@protonmail.com>
2024-01-11 08:28:21 -0500

    Adding updates, project is still unstable/WIP

--- garment.py
+++ garment.py
@@ -3,45 +3,108 @@
 
 # 'Garment' class hierarchy
 class Garment:
-    def __init__(self, rowGauge, colGauge, targetMeas=[], actualMeas=[]):
+    def __init__(self, rowGauge, colGauge, measList=[]):
         self.rowGauge = rowGauge #1in gauge for now
         self.colGauge = colGauge #1in gauge for now
-        self.targetMeas = targetMeas #data structure to hold target Measurement objects
-        self.actualMeas = actualMeas #data structure to hold actual Measurement objects
+        self.measList = measList #data structure to hold Measurement objects
 
     def getGauge(self):
         """ Return stitch size of Measurement object """
         return (self.rowGauge, self.colGauge)
+    
+    def getRowGauge(self):
+        """ Return row gauge """
+        return self.rowGauge
+    
+    def getColGauge(self):
+        """ Return column gauge """
+        return self.colGauge
 
     def setGauge(self, row, col):
-        """ Return inch size of Measurement object """
+        """ Set row & column gauge of garment """
         self.rowGauge = row
         self.colGauge = col
 
-    def getTargetMeas(self):
-        """ Return targetMeas array """
-        return self.targetMeas
+    def setRowGauge(self, row):
+        """ Set row gauge of garment """
+        self.rowGauge = row
 
-    def getActualMeas(self):
-        """ Return actualMeas array """
-        return self.actualMeas
+    def setColGauge(self, col):
+        """ Set column gauge of garment """
+        self.colGauge = col
 
-    def addTarget(self, myName, myBool, mySize):
+    def getMeasList(self):
+        """ Return measList array """
+        return self.measList
+
+    def addTarget(self, myName, myIsWidth, mySize, myDiv=[], myIsOdd=None):
         """ Initialize new Target object & add it to targetMeas array """
-        newTarget = Measurement(myName, myBool, mySize)
-        self.targetMeas.append(newTarget)
+        newTarget = Measurement(myName, myIsWidth, mySize, myDiv, myIsOdd)
+        self.measList.append(newTarget)
 
-    #todo
-    def printTargets():
+    def printTargetMeas(self):
         """ Print out all target measurement information for Garment """
+        for each in self.measList:
+            print(f"{each.getName()}, {each.getTargetSizeIn()} inches, {each.getTargetStitches(self.getRowGauge(), self.getColGauge())} stitches")
+        return self.measList
+    
+    def printActualMeas(self):
+        """ Print out all actual measurement information for Garment """
+        for each in self.measList:
+            print(f"{each.getName()}, {each.getActualSizeIn()} inches, {each.getActualStitches(self.getRowGauge(), self.getColGauge())} stitches")
+        return self.measList
+    
+    def printSummary(self):
+        """ Print summary of all design measurements """
+        for each in self.measList:
+            print(f"{each.getName()}; Stitches {each.getTargetStitches(self.getRowGauge(), self.getColGauge())} -> {each.getActualStitches()};")
+        return self.measList
+    
+    def targetStitchesByName(self, searchName):
+        """ Return the number of stitches in target Measurement by name """
+        for each in self.measList:
+            if searchName == each.getName():
+                return each.getTargetStitches(self.rowGauge, self.colGauge)
         return None
+    
+    def actualStitchesByName(self, searchName):
+        """ Return the number of stitches in target Measurement by name """
+        for each in self.measList:
+            if searchName == each.getName():
+                return each.getActualStitches()
+        return None
+    
+    def targetSizeByName(self, searchName):
+        """ Return the size in inches of target Measurement by name """
+        for each in self.measList:
+            if searchName == each.getName():
+                return each.getTargetSizeIn()
+        return None
+    
+    def actualSizeByName(self, searchName):
+        """ Return the size in inches of actual Measurement by name """
+        for each in self.measList:
+            if searchName == each.getName():
+                return each.getActualSizeIn()
+        return None
+    
+    def calculateMeasurements(self, rowGauge, colGauge):
+        """ Calculate actualSize_in values for all Measurement objects stored in Garment.targetMeas """
+        for each in self.measList:
+            each.calculateMeasurement(rowGauge, colGauge)
        
 # 'Measurement' class hierarchy
 class Measurement:
-    def __init__(self, name, isWidth, inchSize):
+
+    actualSize_in = None
+    actualStitches = None
+
+    def __init__(self, name, isWidth, targetSize_in, divBy, isOdd):
         self.name = name
-        self.isWidth = isWidth #true for width measurement
-        self.inchSize = inchSize
+        self.isWidth = isWidth #true if width measurement, false if height
+        self.targetSize_in = targetSize_in
+        self.divBy = divBy #list of numbers that measurement must be divisible by
+        self.isOdd = isOdd #true if measurement must by odd
 
     def setName(self, newName):
         """ Set the name variable """
@@ -51,9 +114,13 @@ class Measurement:
         """ Set the isWidth variable """
         self.isWidth = newBool
 
-    def setInchSize(self, newSize):
-        """ Set the inchSize variable """
-        self.inchSize = newSize
+    def setTargetSizeIn(self, newSize):
+        """ Set the targetSize_in variable """
+        self.targetSize_in = newSize
+
+    def setActualSizeIn(self, newSize):
+        """ Set the actualSize_in variable """
+        self.actualSize_in = newSize
 
     def getName(self):
         """ Return the name variable """
@@ -63,15 +130,69 @@ class Measurement:
         """ Return the isWidth variable """
         return self.isWidth
 
-    def getInchSize(self):
+    def getTargetSizeIn(self):
         """ Return the inchSize variable """
-        return self.inchSize
+        return self.targetSize_in
+    
+    def getDivBy(self):
+        """ Return the divBy variable """
+        return self.divBy
+    
+    def getIsOdd(self):
+        """ Return the isOdd variable """
+        return self.isOdd
+    
+    def getActualSizeIn(self):
+        """ Return the actualSize_in variable """
+        return self.actualSize_in
+    
+    def getActualStitches(self):
+        """ Return the actualStitches variable """
+        return self.actualStitches
 
-    def toStitches(self, rowGauge, colGauge):
-        """ Return stitch size of Measurement object based on gauge """
+    def getTargetStitches(self, rowGauge, colGauge):
+        """ Return stitch size of target design measurement based on gauge """
         if self.isWidth:
-            return self.inchSize/colGauge
+            return self.targetSize_in*colGauge
         elif not self.isWidth:
-            return self.inchSize/rowGauge
+            return self.targetSize_in*rowGauge
         else:
             return None
+        
+    def calculateMeasurement(self, rowGauge , colGauge, roundUp=True):
+        """ Update actual measurement based on current divisibility requirements """
+        stitch_num = self.getTargetStitches(rowGauge, colGauge)
+
+        #If required, add 1 stitch to an even target
+        #if self.isOdd and stitch_num % 2 == 1:
+            #stitch_num = stitch_num + 1
+
+        #Simplify divisibiliy list
+        del_list = []
+        for value1 in self.divBy:
+            for value2 in self.divBy:
+                if value1 != value2 and value1 % value2 == 0:
+                    del_list.append(value2)
+
+        for each in del_list:
+            if each in self.divBy:
+                self.divBy.remove(each)
+
+        #Multiply the numbers to get interval
+        mult_num = 1
+        for each in self.divBy:
+            mult_num = each * mult_num
+
+        #Rounding
+        # Case 1: all requirements met
+        if stitch_num % mult_num == 0:
+            self.actualStitches = stitch_num
+        # Case 2: div num is larger than stitch target, need to round up
+        elif mult_num > stitch_num:
+            self.actualStitches = mult_num
+        # Case 3: div num is smaller than stitch target, want to round up
+        elif roundUp: #round up
+            self.actualStitches = stitch_num + (mult_num - (stitch_num % mult_num))
+        # Case 4: div num is smaler than stitch target, want to round down
+        else:
+            self.actualStitches = stitch_num - (stitch_num % mult_num)
--- toast-raglan.py
+++ toast-raglan.py
@@ -4,8 +4,8 @@
 import garment as gar
 
 # User input: 1 inch gauge
-rowGauge = 3
-colGauge = 5
+colGauge = 3
+rowGauge = 5
 
 # User input: Target garment measurements in inches
 full_neck = 23
@@ -18,8 +18,8 @@ arm_length = 14 #without ribbing
 raglan_depth = 8 #without ribbing
 
 # Other calculated or constant target garment measurements
-ribbing_depth = 1.5
-neck_shaping_depth = 1
+ribbing_depth = 2 #same for all sizes
+neck_shaping_depth = 1 #same for all sizes
 front_neck = (1/3)*full_neck
 back_neck = (1/3)*full_neck
 arm_neck = (1/6)*full_neck
@@ -29,9 +29,40 @@ yoke_front = pit_to_pit/2 - under_arm
 
 # Create our objects
 raglan = gar.Garment(rowGauge, colGauge)
-raglan.addTarget("full neck", True, full_neck)
+raglan.addTarget("full neck", True, full_neck, [4]) # %2,4
+raglan.addTarget("pit to pit", True, pit_to_pit, [4]) # %2,4
+raglan.addTarget("arm hole", True, arm_hole)
+raglan.addTarget("under arm", True, under_arm) # odd, 7
+raglan.addTarget("wrist", True, wrist, [4]) # %2,4
+raglan.addTarget("body length", False, body_length)
+raglan.addTarget("arm length", False, arm_length)
+raglan.addTarget("raglan depth", False, raglan_depth)
+raglan.addTarget("ribbing depth", False, ribbing_depth)
+raglan.addTarget("neck shaping depth", False, neck_shaping_depth)
+raglan.addTarget("front neck", True, front_neck)
+raglan.addTarget("back neck", True, back_neck)
+raglan.addTarget("arm neck", True, arm_neck)
+raglan.addTarget("full yoke", True, full_yoke)
+raglan.addTarget("yoke arm", True, yoke_arm)
+raglan.addTarget("yoke front", True, yoke_front)
 
-# Pattern math
+#Do pattern math
+print("Calculating Actual Design Measurements from Targets:")
+raglan.calculateMeasurements(rowGauge, colGauge)
+raglan.printSummary()
+
+
+# Error calculation
 
 # Pattern text
+pattern_text = []
+pattern_text.append(f"Using smaller needles, CO {raglan.targetStitchesByName("pit to pit")} in the round using long-tail method.")
+pattern_text.append(f"Row 1-5: Knit all stitches.")
+pattern_text.append(f"Row 6-11: K2, P2 ribbing.")
+pattern_text.append(f"Row 12: *P1, K3, P1, K3, P1, K{raglan.targetStitchesByName("pit to pit")/2-9}* repeat once.")
+pattern_text.append(f"Repeat row 12 until body measures {raglan.targetSizeByName("body length")+raglan.targetSizeByName("ribbing depth")} inches with stockinette after CO held uncurled.")
 
+print()
+print("PATTERN TEXT:")
+for each in pattern_text:
+    print(each)
